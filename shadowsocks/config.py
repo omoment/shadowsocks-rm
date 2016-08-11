bdcd3cb324ef4b1af7243e360232504aa16ddba9
Acris Liu <acrisliu@gmail.com>
2016-08-11 11:32:21 +0800

    remove ss-panel mu function

--- shadowsocks/config.py
+++ shadowsocks/config.py
@@ -15,14 +15,6 @@ MANAGE_PASS = 'passwd'
 MANAGE_BIND_IP = '127.0.0.1'
 #make sure this port is idle
 MANAGE_PORT = 23333
-
-PANEL_VERSION = 'V3' # V2 or V3. V2 not support API
-API_URL = 'http://domain/mu'
-API_PASS = 'mupass'
-NODE_ID = '1'
-CHECKTIME = 15
-SYNCTIME = 600
-
 #BIND IP
 #if you want bind ipv4 and ipv6 '[::]'
 #if you want bind all of ipv4 if '0.0.0.0'
@@ -34,4 +26,3 @@ SS_METHOD = 'rc4-md5'
 LOG_ENABLE = False
 LOG_LEVEL = logging.DEBUG
 LOG_FILE = '/var/log/shadowsocks.log'
-
